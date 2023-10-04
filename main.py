784b1579ed74ecae5b8d6af7fe9d81fc451b6967
jamietwells <jtwells3@googlemail.com>
2023-10-04 18:14:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-beginArray = '['
-beginObject = '{'
-endArray = ']'
-endObject = '}'
-nameSeparator = ':'
-valueSeparator = ';'
+begin_array = '['
+begin_object = '{'
+end_array = ']'
+end_object = '}'
+name_separator = ':'
+value_separator = ';'
 whitespace = [' ', '\t', '\n', '\r']
 
 class JsonToken:
@@ -67,7 +67,7 @@ def parse_any_of(chars, input_string):
         return first_char, input_string[1:]  # Return the matched char and remaining string
     else:
         return None, input_string  # No match found
-     
+
 return_parser = lambda parser: lambda result_formatter: lambda input_string: parser(result_formatter, input_string)
 
 def and_combinator(*parsers):
@@ -97,7 +97,20 @@ def or_combinator(*parsers):
         return None, input_string
 
     return return_parser(combined_parser)
-    
+
+def many_combinator(parser):
+    def combined_parser(result_formatter, input_string):
+        results = []
+        current_input = input_string
+
+        while True:
+            result, current_input = parser(current_input)
+            if result is None:
+                return results, current_input
+            results.append(result)
+
+    return return_parser(combined_parser)
+
 # Define the individual parsers
 parse_a = lambda s: parse_char('a', s)
 parse_e = lambda s: parse_char('e', s)
@@ -114,14 +127,23 @@ parse_true = and_combinator(parse_t, parse_r, parse_u, parse_e)(lambda _: JsonTr
 parse_false = and_combinator(parse_f, parse_a, parse_l, parse_s, parse_e)(lambda _: JsonFalseLiteral())
 parse_null = and_combinator(parse_n, parse_u, parse_l, parse_l)(lambda _: JsonNullLiteral())
 
+
 # Combine them using the or_combinator to parse the json literal
 parse_literal = or_combinator(parse_true, parse_false, parse_null)(lambda r: r)
+
+parse_whitespace = lambda s: parse_any_of(whitespace, s)
+
+#parse_string = and_combinator(lambda s: parse_char('"', s), parse_string_internal, lambda s: parse_char('"', s))
+
+parse_throw_whitespace = many_combinator(parse_whitespace)(lambda _: None)
+
+parse_json = and_combinator(parse_throw_whitespace, parse_literal, parse_throw_whitespace)(lambda r: r[1])
 
 def printResult(output):
     result, remaining = output
     print(repr(result))
-
-printResult(parse_literal("apple"))
-printResult(parse_literal("true"))
-printResult(parse_literal("false"))
-printResult(parse_literal("null"))
+    
+printResult(parse_json(" apple "))
+printResult(parse_json(" true "))
+printResult(parse_json("   false "))
+printResult(parse_json(" null "))
